4296d9261cc213a5ea1a9321a9e3e24eedb605b9
krystianrola <krystianrola96@gmail.com>
2025-10-07 19:41:22 +0200

    created type for discounts

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -5,5 +5,12 @@ export const ButtonVariant = {
   Create: "create",
   Delete: "delete",
 };
-
 export type TButtonVariant = (typeof ButtonVariant)[keyof typeof ButtonVariant];
+
+export const DiscountTypes = {
+  Percentage: "percentage", //-20%
+  Fixed: "fixed", // 20eu off
+  BuyOneGetOne: "bogo", // 2+1
+  Bundle: "bundle", // 2 for xEUR
+};
+export type TDiscountType = (typeof DiscountTypes)[keyof typeof DiscountTypes];
